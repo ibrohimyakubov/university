5bb325493351ed34465fc5e98ab2d59b8c1fdd4e
ibrohim <>
2021-06-09 16:37:43 +0500

    commit

--- university/settings.py
+++ university/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3(q@!i5=-2+1km07ucsb$k9=rycx*t4auxm%zbj&ui$+=v1m%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
--- university/urls.py
+++ university/urls.py
@@ -1,12 +1,11 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from djano.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
